3d694d4c159db93934948b1817458002310d7434
José Luna <pepejose@outlook.com>
2022-02-16 10:49:07 -0500

    filter from response

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ extern crate reqwest;
 extern crate scraper;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
-use std::time::Instant;
+use std::io;
 
 use scraper::{Html, Selector};
 
-fn filter_by_incorrect_words(words: Vec<String>, incorrect_words: Vec<String>) -> Vec<String> {
+fn filter_by_incorrect_words(words: &Vec<String>, incorrect_words: &Vec<String>) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     let mut incorrect_letters: HashSet<String> = HashSet::new();
 
@@ -37,7 +37,7 @@ fn filter_by_incorrect_words(words: Vec<String>, incorrect_words: Vec<String>) -
         }
 
         if can_show.iter().all(|(_, can)| can == &true) {
-            response.push(word);
+            response.push(word.to_string());
         }
 
         for val in can_show.values_mut() {
@@ -47,19 +47,19 @@ fn filter_by_incorrect_words(words: Vec<String>, incorrect_words: Vec<String>) -
     response
 }
 
-fn filter_by_letter(words: Vec<String>, letter: &str) -> Vec<String> {
+fn filter_by_letter(words: &Vec<String>, letter: &str) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     for word in words {
         let mut word_split: Vec<&str> = word.split("").collect();
         word_split.retain(|&s| s != String::from(""));
         if word_split.contains(&letter) {
-            response.push(word)
+            response.push(word.to_string())
         }
     }
     response
 }
 
-fn includes_letters(words: Vec<String>, letters: Vec<String>) -> Vec<String> {
+fn includes_letters(words: &Vec<String>, letters: &Vec<String>) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     let mut checks = vec![false; letters.len()];
     for word in words {
@@ -69,45 +69,45 @@ fn includes_letters(words: Vec<String>, letters: Vec<String>) -> Vec<String> {
             }
         }
         if checks.iter().all(|&x| x == true) {
-            response.push(word)
+            response.push(word.to_string())
         }
         checks = vec![false; letters.len()];
     }
     response
 }
 
-fn starts_with(words: Vec<String>, letter: &str) -> Vec<String> {
+fn starts_with(words: &Vec<String>, letter: &str) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     for word in words {
         if word.starts_with(letter) {
-            response.push(word)
+            response.push(word.to_string())
         }
     }
     response
 }
 
-fn ends_with(words: Vec<String>, letter: &str) -> Vec<String> {
+fn ends_with(words: &Vec<String>, letter: &str) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     for word in words {
         if word.ends_with(letter) {
-            response.push(word)
+            response.push(word.to_string())
         }
     }
     response
 }
 
-fn find_with_static_letters(words: Vec<String>, letters: Vec<String>) -> Vec<String> {
+fn find_with_static_letters(words: &Vec<String>, letters: &Vec<String>) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     let letter_to_look = letters.join("");
     for word in words {
         if word.contains(&letter_to_look) {
-            response.push(word)
+            response.push(word.to_string())
         }
     }
     response
 }
 
-fn find_with_pattern(words: Vec<String>, letters: Vec<String>) -> Vec<String> {
+fn find_with_pattern(words: &Vec<String>, letters: &Vec<String>) -> Vec<String> {
     let mut response: Vec<String> = Vec::new();
     let letters_to_look = letters.join("");
     let mut match_pattern_arr = [false; 5];
@@ -127,7 +127,7 @@ fn find_with_pattern(words: Vec<String>, letters: Vec<String>) -> Vec<String> {
         }
 
         if match_pattern_arr.iter().all(|v| *v) {
-            response.push(word)
+            response.push(word.to_string())
         }
         match_pattern_arr = [false; 5]
     }
@@ -158,7 +158,7 @@ async fn query_web_scrapping(
     Ok(word_list)
 }
 
-async fn get_words(lang: Langs) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+async fn get_words<'l>(lang: &'l Langs) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let query_values = match lang {
         Langs::En => QueryObject {
             url: String::from("https://wordfind.com/length/5-letter-words"),
@@ -195,43 +195,46 @@ async fn get_words(lang: Langs) -> Result<Vec<String>, Box<dyn std::error::Error
     }
 }
 
-async fn query_words(
-    input: WordleCLI,
-    wordle_filtered: Vec<String>,
+async fn query_words<'l>(
+    input: &WordleCLI<'l>,
+    wordle_filtered: Option<&'l Vec<String>>,
 ) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let word_list: Vec<String>;
-    if wordle_filtered.len() == 0 {
-        word_list = match get_words(input.lang).await {
-            Ok(value) => value,
-            Err(_) => vec![String::new()],
+
+    match wordle_filtered {
+        Some(list) => word_list = list.clone(),
+        None => {
+            let queried_words = match get_words(input.lang).await {
+                Ok(value) => value,
+                Err(_) => panic!("this is a terrible mistake!"),
+            };
+            word_list = queried_words;
         }
-    } else {
-        word_list = wordle_filtered
     }
 
     let response: Vec<String>;
 
     match input.action {
         Actions::FirstLetter => {
-            response = starts_with(word_list, &input.clean_input[0]);
+            response = starts_with(&word_list, &input.clean_input[0]);
         }
         Actions::LastLetter => {
-            response = ends_with(word_list, &input.clean_input[0]);
+            response = ends_with(&word_list, &input.clean_input[0]);
         }
         Actions::Contain => {
-            response = filter_by_letter(word_list, &input.clean_input[0]);
+            response = filter_by_letter(&word_list, &input.clean_input[0]);
         }
         Actions::Contains => {
-            response = includes_letters(word_list, input.clean_input);
+            response = includes_letters(&word_list, &input.clean_input);
         }
         Actions::IncorrectWords => {
-            response = filter_by_incorrect_words(word_list, input.clean_input);
+            response = filter_by_incorrect_words(&word_list, &input.clean_input);
         }
         Actions::StaticLetters => {
-            response = find_with_static_letters(word_list, input.clean_input);
+            response = find_with_static_letters(&word_list, &input.clean_input);
         }
         Actions::Pattern => {
-            response = find_with_pattern(word_list, input.clean_input);
+            response = find_with_pattern(&word_list, &input.clean_input);
         }
         Actions::NoAction => response = vec![String::from("no action required")],
     }
@@ -256,7 +259,7 @@ struct WordleCLI<'l> {
     lang: &'l Langs,
 }
 
-impl WordleCLI<'l, T> {
+impl<'l> WordleCLI<'l> {
     pub fn new(action: Actions, raw_input: &String, lang: &'l Langs) -> Self {
         WordleCLI {
             clean_input: WordleCLI::clean_raw_input(&action, String::from(raw_input)),
@@ -305,7 +308,7 @@ impl fmt::Display for Actions {
     }
 }
 
-impl fmt::Display for WordleCLI {
+impl<'l> fmt::Display for WordleCLI<'l> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
@@ -331,10 +334,9 @@ impl fmt::Display for Langs {
 
 #[tokio::main]
 async fn main() {
-    let start = Instant::now();
     let lang_input = std::env::args().nth(1).expect("no lang given");
-    let action = std::env::args().nth(2).expect("no action given");
-    let input = std::env::args().nth(3).expect("no input given");
+    let mut action = std::env::args().nth(2).expect("no action given");
+    let mut input = std::env::args().nth(3).expect("no input given");
 
     let mut wordle_obj: WordleCLI;
     let lang: Langs = match lang_input.as_str() {
@@ -343,32 +345,60 @@ async fn main() {
         _ => Langs::En,
     };
 
-    let first_time = true;
+    let mut first_time = true;
+    let mut filtered_words: Vec<String> = vec![String::new()];
 
     loop {
+        if !first_time {
+            let mut line = String::new();
+
+            io::stdin()
+                .read_line(&mut line)
+                .ok()
+                .expect("Failed to read line");
+            let values = line.split(" ").collect::<Vec<&str>>();
+
+            action = values[0].to_string();
+            input = String::from(values[1].to_string().trim_end());
+        }
+
         match action.as_str() {
             "firstLetter" => wordle_obj = WordleCLI::new(Actions::FirstLetter, &input, &lang),
-            "lastLetter" => wordle_obj = WordleCLI::new(Actions::LastLetter, input, lang),
-            "containsLetters" => wordle_obj = WordleCLI::new(Actions::Contains, input, lang),
-            "containSingleLetter" => wordle_obj = WordleCLI::new(Actions::Contain, input, lang),
+            "lastLetter" => wordle_obj = WordleCLI::new(Actions::LastLetter, &input, &lang),
+            "containsLetters" => wordle_obj = WordleCLI::new(Actions::Contains, &input, &lang),
+            "containSingleLetter" => wordle_obj = WordleCLI::new(Actions::Contain, &input, &lang),
             "filterByIncorrectWords" => {
-                wordle_obj = WordleCLI::new(Actions::IncorrectWords, input, lang)
+                wordle_obj = WordleCLI::new(Actions::IncorrectWords, &input, &lang)
             }
-            "pattern" => wordle_obj = WordleCLI::new(Actions::Pattern, input, lang),
-            "staticLetters" => wordle_obj = WordleCLI::new(Actions::StaticLetters, input, lang),
-            _ => wordle_obj = WordleCLI::new(Actions::NoAction, String::from(" :( "), lang),
+            "pattern" => wordle_obj = WordleCLI::new(Actions::Pattern, &input, &lang),
+            "staticLetters" => wordle_obj = WordleCLI::new(Actions::StaticLetters, &input, &lang),
+            _ => wordle_obj = WordleCLI::new(Actions::NoAction, &String::from(" :( "), &lang),
         };
-        println!("{}", wordle_obj);
-        match query_words(wordle_obj, vec![]).await {
+
+        match wordle_obj.action {
+            Actions::NoAction => {
+                println!("finishing process");
+                break;
+            }
+            _ => println!("{}", wordle_obj),
+        }
+
+        let filtered_option: Option<&Vec<String>>;
+        if first_time {
+            filtered_option = None;
+        } else {
+            filtered_option = Some(&filtered_words)
+        }
+
+        match query_words(&wordle_obj, filtered_option).await {
             Ok(response) => {
-                println!("{:?}", response)
+                filtered_words = response;
+                println!("{:?}", &filtered_words)
             }
             Err(_) => {
                 println!("error")
             }
         }
-        let duration = start.elapsed();
-        println!("Time elapsed {:?}", duration);
         first_time = false;
     }
 }
